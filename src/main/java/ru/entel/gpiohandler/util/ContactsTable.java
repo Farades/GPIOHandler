8c235fde9ec64a7e4f8196a6980d07c158f702d4
farades <creg2007@gmail.com>
2015-11-09 14:17:08 +0300

    add ContactsTable pinout smiu->RPi, first worked version

--- src/main/java/ru/entel/gpiohandler/util/ContactsTable.java
+++ src/main/java/ru/entel/gpiohandler/util/ContactsTable.java
@@ -11,17 +11,17 @@ public class ContactsTable {
 
     static {
         smiuPitable = new HashMap<>();
-        smiuPitable.put(SmiuGPIO.INPUT_1, RaspiPin.GPIO_00);
-        smiuPitable.put(SmiuGPIO.INPUT_2, RaspiPin.GPIO_01);
-        smiuPitable.put(SmiuGPIO.INPUT_3, RaspiPin.GPIO_02);
-        smiuPitable.put(SmiuGPIO.INPUT_4, RaspiPin.GPIO_03);
+        smiuPitable.put(SmiuGPIO.INPUT_1, RaspiPin.GPIO_23);
+        smiuPitable.put(SmiuGPIO.INPUT_2, RaspiPin.GPIO_06);
+        smiuPitable.put(SmiuGPIO.INPUT_3, RaspiPin.GPIO_12);
+        smiuPitable.put(SmiuGPIO.INPUT_4, RaspiPin.GPIO_05);
         smiuPitable.put(SmiuGPIO.INPUT_5, RaspiPin.GPIO_04);
-        smiuPitable.put(SmiuGPIO.INPUT_6, RaspiPin.GPIO_05);
-        smiuPitable.put(SmiuGPIO.INPUT_7, RaspiPin.GPIO_06);
-        smiuPitable.put(SmiuGPIO.INPUT_8, RaspiPin.GPIO_07);
-        smiuPitable.put(SmiuGPIO.INPUT_9, RaspiPin.GPIO_08);
-        smiuPitable.put(SmiuGPIO.INPUT_10, RaspiPin.GPIO_09);
-        smiuPitable.put(SmiuGPIO.INPUT_11, RaspiPin.GPIO_10);
-        smiuPitable.put(SmiuGPIO.INPUT_12, RaspiPin.GPIO_11);
+        smiuPitable.put(SmiuGPIO.INPUT_6, RaspiPin.GPIO_01);
+        smiuPitable.put(SmiuGPIO.INPUT_7, RaspiPin.GPIO_29);
+        smiuPitable.put(SmiuGPIO.INPUT_8, RaspiPin.GPIO_10);
+        smiuPitable.put(SmiuGPIO.INPUT_9, RaspiPin.GPIO_11);
+        smiuPitable.put(SmiuGPIO.INPUT_10, RaspiPin.GPIO_21);
+        smiuPitable.put(SmiuGPIO.INPUT_11, RaspiPin.GPIO_26);
+        smiuPitable.put(SmiuGPIO.INPUT_12, RaspiPin.GPIO_22);
     }
 }
